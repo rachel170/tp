0977c9dfa3bde2d51f39edcc86a6f60fc6a24ecb
rachel170 <e0418127@u.nus.edu>
2020-10-23 00:24:58 +0800

    Test: Add EndReviewCommandTest

---
+++ src/test/java/seedu/flashnotes/logic/commands/EndReviewCommandTest.java
@@ -0,0 +1,21 @@
+package seedu.flashnotes.logic.commands;
+
+import static seedu.flashnotes.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.flashnotes.logic.commands.EndReviewCommand.MESSAGE_EXIT_ACKNOWLEDGEMENT;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.flashnotes.model.Model;
+import seedu.flashnotes.model.ModelManager;
+
+public class EndReviewCommandTest {
+    private Model model = new ModelManager();
+    private Model expectedModel = new ModelManager();
+
+    @Test
+    public void execute_endReview_success() {
+        CommandResult expectedCommandResult = new CommandResult(EndReviewCommand.MESSAGE_EXIT_ACKNOWLEDGEMENT,
+                false, true, false, false, 0);
+        assertCommandSuccess(new EndReviewCommand(), model, expectedCommandResult, expectedModel);
+    }
+}
